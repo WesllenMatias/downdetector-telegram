29ece5dde9abe1f2538f23c4115867aff432843b
Wesllen Matias Maia <wesllen.maia2010@gmail.com>
2020-04-27 13:12:53 -0300

    Alterada a lib de telepot para telebot

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-BotTelegram Downdetector
+# BotTelegram Downdetector
+
+
+Passo a Passo para usar o Bot:
+
+
+## Instalando dependências
+
+__São utilizadas algumas bibliotecas  que precisam ser instaladas:__
+
+- Selenium
+- pyTelegramBotAPI
+- BeautifulSoup
+
+__-OBS: O python utilizado é o Python3__
+
+```shellscript
+
+pip3 install selenium
+
+pip3 install pyTelegramBotAPI
+
+pip3 install bs4
+
+```
+
+## Clone o Repositório
+
+```shellscript
+
+git clone https://github.com/WesllenMatias/downdetector-telegram.git
+
+```
+## Crie o arquivo chamado config.py com o Token e o ID de quem vai receber a mensagem:
+
+```Python
+
+TOKEN = "Informe o Token do seu bot aqui"
+USER = "Informe aqui o ID de quem vai receber a mensagem."
+
+```
+
+## 
--- downdetector.py
+++ downdetector.py
@@ -1,35 +1,61 @@
 # -*- coding: utf-8 -*-
+# downdetector.py - Informar o Top5 do Downdetector
+#
+# Site:       https://wesllenmatias.github.io
+# Autor:      Wesllen Matias
+# Manutenção: Wesllen Matias
+# Telegram: @wesllenmatias
+#
+# ------------------------------------------------------------------------ #
+# Este programa vai checar os Top 5 do Downdetector e enviar via telegram
+#
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 - Versão inicial
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   Python 3.6.9
+# ------------------------------------------------------------------------ #
 from requests import get
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
-import telepot
+from selenium.webdriver.firefox.options import Options
+import telebot
 import config
 
 base_url = 'https://downdetector.com.br/'
 #link = f"{base_url}/fora-do-ar/"
+bot = telebot.TeleBot(config.TOKEN)
 
-bot = telepot.Bot(config.TOKEN)
-browser = webdriver.Firefox()
-browser.get(base_url)
-downdetector = browser.page_source
-downdetector_page = bs(downdetector,'html.parser')
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Olá Bem-Vindo ao seu Bot!")
 
-problemas = downdetector_page.find_all('h5')
-browser.close()
-lista = []
+@bot.message_handler(commands=['foradoar'])
+def downdetector_foradoar (message):
+    options = Options()
+    options.headless = True
+    browser = webdriver.Firefox(options=options)
+    browser.get(base_url)
+    downdetector = browser.page_source
+    downdetector_page = bs(downdetector,'html.parser')
 
-for problema in problemas:
-    titulo = problema
-    lista.append(str(titulo).strip('<h5>'))
+    problemas = downdetector_page.find_all('h5')
+    browser.close()
+    lista = []
 
-n1 = lista[0].strip('</')
-n2 = lista[1].strip('</')
-n3 = lista[2].strip('</')
-n4 = lista[3].strip('</')
-n5 = lista[4].strip('</')
+    for problema in problemas:
+        titulo = problema
+        lista.append(str(titulo).strip('<h5>'))
 
-msg = "Top 5 Downdetector:\n\n 1 - {}\n 2 - {}\n 3 - {}\n 4 - {}\n 5 - {}\n".format(n1,n2,n3,n4,n5)
-bot.sendMessage(config.USER,msg)
+    n1 = lista[0].strip('</')
+    n2 = lista[1].strip('</')
+    n3 = lista[2].strip('</')
+    n4 = lista[3].strip('</')
+    n5 = lista[4].strip('</')
 
+    msg = "Top 5 Downdetector:\n\n 1 - {}\n 2 - {}\n 3 - {}\n 4 - {}\n 5 - {}\n".format(n1,n2,n3,n4,n5)
+    bot.reply_to(message,msg)
 
-print ("Top 5 Downdetector:\n\n{}\n {}\n {}\n {}\n {}\n".format(n1,n2,n3,n4,n5))
+bot.polling()
